b637f0c2cd3904816c5dffcb533cf132b780698e
Abdulrahman Taha <ghazalec027@gmail.com>
2026-08-30 14:55:32 +0300

    fix: correct send email utility

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,1 +1,28 @@
-git status
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
+
+  auth: {
+    user: process.env.SMTP_LOGIN,
+    pass: process.env.SMTP_KEY,
+  },
+});
+
+const sendEmail = async ({ to, subject, html, text }) => {
+  const mailOptions = {
+    from: `"${process.env.EMAIL_FROM_NAME}" <${process.env.EMAIL_FROM}>`,
+    to,
+    subject,
+    text,
+    html,
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+
+  return info;
+};
+
+export default sendEmail;
