e84b2e548dd7387fa2455883daaa034556f59ffc
ziad-wdev <ziadahmed2371@gmail.com>
2026-09-06 01:21:39 +0300

    Configure session cookie security settings

--- src/config/constants.js
+++ src/config/constants.js
@@ -23,4 +23,7 @@ export const MODEL_OPTIONS = Object.freeze({
 export const COOKIE_OPTIONS = Object.freeze({
   httpOnly: true,
   secure: environment.isProduction,
+  sameSite: 'strict',
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  path: '/',
 })
