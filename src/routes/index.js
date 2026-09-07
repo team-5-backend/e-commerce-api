a59cde3813e7a5a45ff6c7cc6d18d16c11041687
ziad-wdev <ziadahmed2371@gmail.com>
2026-09-07 08:30:17 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 import { HTTP_STATUS } from '../config/constants.js'
 
+import adminRoutes from './admin.routes.js'
 import authRoutes from './auth.routes.js'
 
 const router = express.Router()
@@ -14,5 +15,6 @@ router.get('/health', (_req, res) =>
 )
 
 router.use('/auth', authRoutes)
+router.use('/admin', adminRoutes)
 
 export default router
