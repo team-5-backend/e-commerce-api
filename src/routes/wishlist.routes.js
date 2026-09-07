b1086deffb73693c11a1aac78d0ed69e154c1f32
tasneemzakaria-create <tasnemzkaria@gmail.com>
2026-09-07 23:51:36 +0300

    Add wishlist routes with CRUD operations

---
+++ src/routes/wishlist.routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import {
+  addToWishlist,
+  clearWishlist,
+  getMyWishlist,
+  removeFromWishlist
+} from "../controllers/wishlist.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { validateParams } from "../middlewares/validate.middleware.js";
+import { productIdParamsSchema } from "../validations/wishlist.validation.js";
+
+const router = Router();
+
+router.use(authenticate);
+
+router.get("/my", getMyWishlist);
+router.post(
+  "/add/:productId",
+  validateParams(productIdParamsSchema),
+  addToWishlist
+);
+router.delete(
+  "/remove/:productId",
+  validateParams(productIdParamsSchema),
+  removeFromWishlist
+);
+router.delete("/clear", clearWishlist);
+
+export default router;
