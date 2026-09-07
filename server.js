4713b48c8c7a4de9702e36e8245e41cf1f480582
ziad-wdev <ziadahmed2371@gmail.com>
2026-09-07 08:43:45 +0300

    Remove unused redisClient import in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import app from './src/app.js'
 import environment from './src/config/environment.js'
 import { connectDatabase, disconnectDatabase } from './src/db/db.js'
-import redisClient, { connectRedis, disconnectRedis } from './src/redis/redisClient.js'
+import { connectRedis, disconnectRedis } from './src/redis/redisClient.js'
 import logger from './src/utils/logger.js'
 
 dotenv.config()
